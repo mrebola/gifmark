d3bc935cebbbb4c1879334c7a7aa833cdba593cf
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2013-02-15 14:52:21 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifmark::Application.config.secret_token = 'a5d1ce64e2754024f5272800053d988ad15e0f3dba4bf80085f639a5eeaa371cb17c9f6dc8554fd7de7e52999898e14c47e94f40b9233ccc5a51bc94997cc757'
