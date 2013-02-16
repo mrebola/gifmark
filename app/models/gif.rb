dd97a020247c067a7d2130023cadbada884af4d6
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2013-02-15 19:39:04 -0600

    modify model for search and add index to table on migration

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,3 +1,18 @@
+require 'open-uri'
+require 'digest/sha1'
+
 class Gif < ActiveRecord::Base
-  attr_accessible :checksum, :titile, :url
+  attr_accessible :title, :url
+  validates :url, presence: true
+  validates :url, :checksum, uniqueness: true
+
+  before_save :calculate_checksum, on: :create
+
+  def self.search query
+  	basic_search(title: query)  	
+  end
+
+  def calculate_checksum
+  	self.checksum = Digest::SHA1.hexdigest( open(url) {|file| file.read } )
+  end
 end
--- db/migrate/20130215211949_create_gifs.rb
+++ db/migrate/20130215211949_create_gifs.rb
@@ -1,11 +1,13 @@
 class CreateGifs < ActiveRecord::Migration
   def change
     create_table :gifs do |t|
-      t.string :titile
+      t.string :title
       t.string :url
       t.string :checksum
+      t.string :response_code
 
       t.timestamps
     end
+    add_index :gifs, :title
   end
 end
